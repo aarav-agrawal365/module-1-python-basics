312d5b149c9ae7c2facf6f73342d34a9cbcd887a
aarav-agrawal365 <priti.satna@gmail.com>
2026-05-11 17:23:33 +0530

    second commit

---
+++ BirthdayDates.py
@@ -0,0 +1,12 @@
+a="Aarav",23,"october",2013
+b="Raghav",27,"january",2016
+c="Kapil",1,"july",2012
+d="Prahlad",7,"february",2013
+e="Krishna",19,"may",2012
+
+print("BIRTHDAY DATES\n")
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
