1ada20c9997a91e2e3a702886ac9b34024bb7063
aarav-agrawal365 <priti.satna@gmail.com>
2026-05-21 15:38:44 +0530

    commit8

---
+++ __pycache__/AndOr.py
@@ -0,0 +1,9 @@
+a=30
+b=23
+c=10
+
+if a>b or b>a:
+    print("I am using OR operater")
+
+elif c<b and a>b:
+    print("I am using AND operater")
---
+++ __pycache__/NotEqualTo.py
@@ -0,0 +1,18 @@
+a=5
+b=23
+c=23
+
+print(a !=b)
+print(b==c)
+
+a="python"
+b="coding"
+
+if a !=b:
+    print(a,"is not equal to",b)
+
+a=33
+b=22
+
+if (a==2) != (b==11*2):
+    print("hello")
