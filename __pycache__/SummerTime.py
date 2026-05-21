6cff01adfd4b830c29fcc13aea16100738f1ae1d
aarav-agrawal365 <priti.satna@gmail.com>
2026-05-21 12:32:08 +0530

    commit7

---
+++ __pycache__/SummerTime.py
@@ -0,0 +1,6 @@
+time=input("Please select one of the options with regards of the weather around you: cold, hot \n")
+if time=="cold":
+    print("Wear thick, warm and woolen clothes which keep you warm and comfortable throughout your day.")
+
+if time=="hot":
+    print("Wear thin, frail and cotton clothes which keep you cool.")
