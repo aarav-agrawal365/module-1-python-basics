c3155b0f6eb42f10caae36632d0fbca8648f4346
aarav-agrawal365 <priti.satna@gmail.com>
2026-06-05 19:22:34 +0530

    commit 11

---
+++ __pycache__/swapping.py
@@ -0,0 +1,13 @@
+a=1
+b=2
+c=3
+print("A before swapping = ", a)
+print("B before swapping = ", b)
+print("C before swapping = ", c)
+
+d=b
+e=c
+f=a
+print("b now swapped to d ",d)
+print("c now swapped to e ",e)
+print("a now swapped to f ",f)
